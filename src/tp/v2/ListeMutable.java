8a4dff68f49a772f016f4f251fc4e94d4d7cc4a8
danieldluyz <danieldluyz7@hotmail.com>
2017-11-08 19:38:41 +0100

    ListeMutableCons

--- src/tp/v2/ListeMutable.java
+++ src/tp/v2/ListeMutable.java
@@ -27,13 +27,32 @@ public interface ListeMutable<E> extends Liste<E>{
 
 	public static <E> ListeMutable<E> cons(E t, ListeMutable<E> r){
 		return new ListeMutable<E>() {
-			// TODO Définir les méthodes utiles.			
+			private E tete = t;
+			private ListeMutable<E> reste = r;
+			
+			public E tete() {
+				return tete;
+			}
+			
+			public ListeMutable<E> reste() {
+				return reste;
+			}
+			
+			public void changerReste(ListeMutable<E> reste) {
+				this.reste = reste;
+			}
+			
+			public void changerTete(E tete) {
+				this.tete = tete;
+			}
+			
 		};
 	}
 	
 	public static <E> ListeMutable<E> vide() {
 		return new ListeMutable<E>() {
-			// TODO Définir les méthodes utiles.						
+			
+								
 		};
 	}
 	
