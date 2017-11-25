06f06486b8cc90e76c5e063410e22c7639b872ed
danieldluyz <danieldluyz@192.168.0.101>
2017-11-25 19:05:05 +0100

    CasFileMutable

---
+++ src/tp/filRouge/v5/CasFileMutable.java
@@ -0,0 +1,51 @@
+package tp.filRouge.v5;
+
+import java.util.Iterator;
+
+public class CasFileMutable<E> implements FileMutable<E> {
+
+	@Override
+	public E premier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FileMutable<E> creer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int taille() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void ajouter(E element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void retirer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public FileMutable<E> creerCopie() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
